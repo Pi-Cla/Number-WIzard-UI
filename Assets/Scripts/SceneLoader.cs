1b6a02ddc047b3639ead5224f20fd44cfa189d42
Pi-Cla <pirateclip@protonmail.com>
2019-04-29 13:51:52 -0600

    Comments

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
+    //Sceneloader graps the scene right after it
     public void LoadNextScene() {
          int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
-
+    //This is run to loop the finish screen back to start
     public void LoadStartScene() {
         SceneManager.LoadScene(0);
     }
